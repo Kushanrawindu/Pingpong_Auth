e923d66aad4f40ae77db8c26b04faefcab1a57f3
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-08 15:39:26 +0530

    configure tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/tailwind.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-purple-500">Tailwind css</h1>
+    </div>
+  );
+}
+
+export default App;
