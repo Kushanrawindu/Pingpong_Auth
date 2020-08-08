5b7bab4bf4c7f46e58e93e2e92cf6c77ef0d68ca
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-08 21:10:36 +0530

    submit login form

--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Login extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {email:'', password:'', errors:{}}
+    }
+
+    handleForm = (e) => {
+        e.preventDefault();
+        this.props.history.push("/profile");
+    }
+
+    handleInput = (e) => {
+        e.preventDefault();
+        const name = e.target.name
+        const value = e.target.value
+        this.setState({[name]:value})
+    }
+
     render() {
         return (
             <div className="w-full flex flex-wrap">
                 {/* Login section */}
                 <div className="w-full md:w-1/2 flex flex-col">
                     <div className="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
-                        <a path="/" className="bg-black text-white font-bold text-xl p-4">Logo</a>
+                        <Link to="/" className="bg-black text-white font-bold text-xl p-4">Logo</Link>
                     </div>
 
                     <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
                         <p className="text-center text-3xl">Welcome</p>
-                        <form className="flex flex-col pt-3 md:pt-8">
+                        <form className="flex flex-col pt-3 md:pt-8" onSubmit={this.handleForm}>
                             <div className="flex flex-col pt-4">
                                 <label for="email" className="text-lg">Email</label>
-                                <input type="email" id="email" placeholder="you@email.com" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" ></input>
+                                <input type="email" id="email" placeholder="you@email.com" onChange={this.handleInput} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" ></input>
                             </div>
 
                             <div className="flex flex-col pt-4">
                                 <label for="password" className="text-lg">Password</label>
-                                <input type="password" id="password" placeholder="Password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" ></input>
+                                <input type="password" id="password" placeholder="Password" onChange={this.handleInput} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" ></input>
                             </div>
 
                             <input type="submit" value="Log In" className="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8"></input>
---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div className="flex w-full justify-center">
+                <h1 className="text-lg text-white mt-10 p-5 rounded-lg bg-black">Profile page</h1>
+            </div>
+        )
+    }
+}
